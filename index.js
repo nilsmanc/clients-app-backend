06e3991e097af30ae734d99770d3e6f9b9d110ca
nilsmanc <etereaiberia@gmail.com>
2023-11-10 09:01:18 +0300

    database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ObjectId } = require('mongodb')
 
 const router = express.Router()
 const app = express()
@@ -9,7 +10,35 @@ app.use(express.json())
 app.use(cors())
 app.use('/api/clients', router)
 
+const clientPromise = MongoClient.connect(process.env.DB_URL, {
+  useUnifiedTopology: true,
+  maxPoolSize: 10,
+})
+
+router.use(async (req, res, next) => {
+  try {
+    const client = await clientPromise
+    req.db = client.db('clients_db')
+
+    next()
+  } catch (error) {
+    next(error)
+  }
+})
+
 const port = process.env.PORT || 3000
+
+router.get('/', async (req, res) => {
+  try {
+    const db = req.db
+
+    const clients = await db.collection('clients').find().toArray()
+
+    res.send(clients)
+  } catch (error) {
+    console.log(error)
+  }
+})
 
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`)
